c061f9dfe920257a7b0cb15ba54c0eb6bc2632ac
rb2705111 <156081288+rb2705111@users.noreply.github.com>
2025-12-18 15:35:27 +0000

    fix: move React Query setup to App.jsx to satisfy validator

--- react-query-demo/src/main.jsx
+++ react-query-demo/src/main.jsx
@@ -1,15 +1,10 @@
 // src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import PostsComponent from './components/PostsComponent';
-
-const queryClient = new QueryClient();
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <PostsComponent />
-    </QueryClientProvider>
+    <App />
   </React.StrictMode>
 );
